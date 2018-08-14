6e923ca676c952721bea942f090685d8fa0281fa
U-NEWBERRY\krcm <krcm@40001000211705S.newberry.org>
2018-08-14 13:24:21 -0500

    Second commit

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -3,6 +3,7 @@ import urllib2, base64
 import json
 import sys
 import datetime
+import time
 import argparse
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -17,6 +18,43 @@ ap.add_argument("-p", "--password", required=True,
 args = vars(ap.parse_args())
 
 collection = args["collection"]
+
+def loadremotejson(url):
+	response = urllib2.urlopen(url)
+	data = json.load(response)
+	return data
+
+def GetImages(pointer):
+  collection = args["collection"]
+  img_pointers = []
+  url = 'https://collections.carli.illinois.edu:8443/dmwebservices/index.php?q=dmGetCompoundObjectInfo/%s/%s/json' % (collection, pointer)
+  print "Getting image at %s" % url
+  try:
+    response = loadremotejson(url)
+  except SocketError as e:
+    if e.errno != errno.ECONNRESET:
+      raise
+    pass
+  if 'type' in response:
+    if response['type'] == 'Monograph':
+      node = response['node']
+      nodes = node['node']
+      for n in nodes:
+        pages = n['page']
+        if isinstance(pages, list):
+          for p in pages:
+            img_pointers.append(p['pageptr'])
+        else:
+          img_pointers.append(pages['pageptr'])
+    elif response['page'] and isinstance(response['page'], list):
+      for p in response['page']:
+        img_pointers.append(p['pageptr'])
+    else:
+      img_pointers.append(response['page']['pageptr'])
+    return img_pointers
+  else:
+    return ['[no images]']
+
 
 username = args["username"]
 password = args["password"]
@@ -35,6 +73,7 @@ del_rows = ["CONTENTdm file name","Date created","Date modified","OCLC number","
 
 for row in reader:
   if "o2" not in row["Title"]:
+    row["img_ptr"] = int(row['CONTENTdm number']) - 2
     for d in del_rows:
       del row[d]
     data.append(row)
